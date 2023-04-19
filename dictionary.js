f413e0e796b1a6ec13d68a67124e2a343417446f
Devendra Bhattarai <91841677+Deven2057@users.noreply.github.com>
2023-04-19 19:45:55 +0530

    Create dictionary.js

---
+++ dictionary.js
@@ -0,0 +1,14 @@
+let a = 32
+let b = "one"
+let c = a + b
+console.log(c);
+console.log(typeof c)
+//example
+const dictionary = {
+  home: "place where we live",
+  school: "place where we study",
+  look: "seeing somthing",
+  pen: "instrument that write"
+
+}
+console.log(dictionary.look)
